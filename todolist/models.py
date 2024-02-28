4004490f3a5c4bb37205778614814cb208fe332c
ihortsar <128905338+ihortsar@users.noreply.github.com>
2024-02-28 11:13:59 +0100

    auth problem

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    checked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id) + ". " + self.title
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from todolist.serializers import TodoItemSerializer
+from .models import TodoItem
+from django.core.serializers import serialize
 
-# Create your views here.
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+
+
+class TodoView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        todos = TodoItem.objects.all()
+        serialized = TodoItemSerializer(todos, many=True)
+        return Response(serialized.data)
